d86a664732ad4425c7410382747d84ee539de78f
SI-Abid <siaabid5323@gmail.com>
2023-02-17 20:57:06 +0600

    fixit

--- naftas.cpp
+++ naftas.cpp
@@ -15,24 +15,25 @@ int main()
     int y1 = 100;
     int x2 = 150;
     int y2 = 150;
-    int depth = 20;
+    int depth = 25;
     setcolor(WHITE);
     line(0, midy, maxx, midy);
     line(midx, 0, midx, maxy);
-    int color = 1;
-    for (int i = 1; i <= 18; i++)
+    int color = 0;
+    char colors[16][16] = {"BLACK", "BLUE", "GREEN", "CYAN", "RED", "MAGENTA", "BROWN", "LIGHTGRAY", "DARKGRAY", "LIGHTBLUE", "LIGHTGREEN", "LIGHTCYAN", "LIGHTRED", "LIGHTMAGENTA", "YELLOW", "WHITE"};
+    for (int i = 1; i <= 16; i++)
     {
         setcolor(color);
         setfillstyle(SOLID_FILL, color);
-        double angle = (i * 20) * 3.14159 / 180;
+        double angle = ((i * 22.5) * 3.14159 / 180)+10;
         int x1_ = x1 * cos(angle) - y1 * sin(angle);
         int y1_ = x1 * sin(angle) + y1 * cos(angle);
         int x2_ = x2 * cos(angle) - y2 * sin(angle);
         int y2_ = x2 * sin(angle) + y2 * cos(angle);
         bar3d(midx + x1_, midy - y1_, midx + x2_, midy - y2_, depth, 1);
+        setcolor(WHITE);
+        outtextxy(midx + x1_, midy - y1_, colors[i]);
         color++;
-        if(color == 15)
-            color = 1;
     }
     
     getch();
